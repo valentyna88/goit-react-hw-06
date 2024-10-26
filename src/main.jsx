0923e51b1a838891da7a3820beafbd9520b2ac8c
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-26 19:08:42 +0300

    Add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
