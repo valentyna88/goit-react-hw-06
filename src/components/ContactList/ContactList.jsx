74e39afb4a7df674e2841ae67597f36b5e7e0bc7
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-26 11:41:25 +0300

    Add ContactList component

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,21 @@
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          <Contact
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+            onDelete={onDelete}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
